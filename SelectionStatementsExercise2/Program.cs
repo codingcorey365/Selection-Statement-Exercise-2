5ef0fba91695315164b85e67cf7c1c345d416c3d
Corey Schroeder <codingcorey365@gmail.com>
2023-10-07 16:06:03 -0500

    exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite subject");
+            string userInput = Console.ReadLine();
+
+            switch (userInput.ToLower())
+            {
+                case "math": 
+                    Console.WriteLine("I love math");
+                    break;
+                
+                case "science":
+                    Console.WriteLine("I love science");
+                    break;
+
+                case "history":
+                    Console.WriteLine("I love history");
+                    break;
+
+                case "english":
+                    Console.WriteLine("I love english");
+                    break;
+                    default: Console.WriteLine($"learning is fun, I have not heard of the subject {userInput} before.");
+                        break;
+            }
         }
     }
 }
